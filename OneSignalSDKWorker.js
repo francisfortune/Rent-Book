c863fb1f4b6fc5a0f852acf2a8e8f41755d7dd3a
francisfortune <155053815+francisfortune@users.noreply.github.com>
2026-07-24 10:11:26 +0100

    new changes. the top padding is not yet fixed

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+// OneSignalSDKWorker.js
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+importScripts('/sw.js'); // Imports your app's standard Service Worker
